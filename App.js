cd4bb985a40426b2875ee38be42323709c6cfd3e
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-29 02:33:17 +0630

    add state hooks and event action

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
+	const [enteredGoal, setEnteredGoal] = useState('')
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	}
+
+	const addGoalHandler = () => {
+		console.log(enteredGoal)
+	}
+
   return (
     <View style={styles.screen}>
 			<View style={styles.inputContainer}>
-				<TextInput placeholder='Course Goal' style={styles.input} />
-				<Button title='ADD' />
+				<TextInput placeholder='Course Goal' value={enteredGoal} onChangeText={goalInputHandler} style={styles.input} />
+				<Button title='ADD' onPress={addGoalHandler} />
 			</View>
     </View>
   );
