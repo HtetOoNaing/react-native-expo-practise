2eb0c569042f934be0158e44c21211961be7212e
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-29 02:39:29 +0630

    add goal list

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 	const [enteredGoal, setEnteredGoal] = useState('')
+	const [courseGoals, setCourseGoals] = useState([])
 
 	const goalInputHandler = (enteredText) => {
 		setEnteredGoal(enteredText);
 	}
 
 	const addGoalHandler = () => {
-		console.log(enteredGoal)
+		setCourseGoals( currentGoals => [...currentGoals, enteredGoal])
 	}
 
   return (
@@ -17,6 +18,9 @@ export default function App() {
 			<View style={styles.inputContainer}>
 				<TextInput placeholder='Course Goal' value={enteredGoal} onChangeText={goalInputHandler} style={styles.input} />
 				<Button title='ADD' onPress={addGoalHandler} />
+			</View>
+			<View>
+				{courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
 			</View>
     </View>
   );
